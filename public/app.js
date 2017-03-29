9f51436217347635470213c113c8f91a0ceaba64
3ll3n <ellengrafton01@gmail.com>
2017-03-29 19:04:04 +0100

    initial commit

---
+++ public/app.js
@@ -0,0 +1,44 @@
+var makeRequest = function(url, callback) {
+// create a new xml http request object
+  var request = new XMLHttpRequest();
+
+// set the type of request and the url
+  request.open("GET", url);
+
+// set the callback we want it to use when the request is complete
+  request.onload = callback;
+
+  // send the request
+  request.send();
+};
+
+var requestComplete = function() {
+  if(this.status !== 200) {
+    return;
+  }
+  // grab the response text
+  var jsonString = this.responseText;
+  var albums = JSON.parse(jsonString);
+  
+  populateList(albums);
+};
+
+var populateList = function(albums) {
+
+  var albums = albums.albums.items;
+
+    albums.forEach(function(album) {
+      console.log(album);
+    var li = document.createElement("li");
+    li.innerText = album.name;
+    var ul = document.querySelector("#results");
+    ul.appendChild(li);
+  });
+};
+
+var app = function(){
+  var url = "https://api.spotify.com/v1/search?q=country&type=album";
+    makeRequest(url, requestComplete);
+}
+
+window.onload = app;
